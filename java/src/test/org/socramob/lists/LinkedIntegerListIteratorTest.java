d6b65f7a1d825bab4ebd48d63678a06f1ad96ee0
Andreas Simon <mail@andreas-simon.info>
2013-02-17 22:01:28 +0100

    Added test class for LinkedIntegerList iterator

---
+++ java/src/test/org/socramob/lists/LinkedIntegerListIteratorTest.java
@@ -0,0 +1,15 @@
+package org.socramob.lists;
+
+public class LinkedIntegerListIteratorTest extends ListIteratorTestTemplate {
+
+    @Override
+    protected IntegerList emptyList() {
+        return LinkedIntegerList.emptyList();
+    }
+
+    @Override
+    protected IntegerList listWithItems(Integer... items) {
+        return LinkedIntegerList.withItems(items);
+    }
+
+}
